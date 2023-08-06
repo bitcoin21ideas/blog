de9f4a1870cf3b645066978165597d9a5eb0a79e
Tony <lightning_tony@protonmail.com>
2023-08-07 00:40:15 +0300

    my blog

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function FourOhFour() {
+  return <>
+    <h1>Are you lost?</h1>
+    <Link href="/">
+        Go back home
+    </Link>
+  </>
+}
